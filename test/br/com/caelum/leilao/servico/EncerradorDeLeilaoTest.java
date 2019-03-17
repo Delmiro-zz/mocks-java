59423360e96ce7fefe6c96c3fc2c963497c4f0b8
Diego Delmiro <diegoalbuquerque03@gmail.com>
2019-03-17 18:30:58 -0300

    Usando método never com verify.

--- test/br/com/caelum/leilao/servico/EncerradorDeLeilaoTest.java
+++ test/br/com/caelum/leilao/servico/EncerradorDeLeilaoTest.java
@@ -1,17 +1,16 @@
 package br.com.caelum.leilao.servico;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
-
+import static org.mockito.Mockito.when;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
-
 import org.junit.Test;
-
 import br.com.caelum.leilao.builder.CriadorDeLeilao;
 import br.com.caelum.leilao.dominio.Leilao;
 import br.com.caelum.leilao.infra.dao.LeilaoDao;
@@ -55,4 +54,26 @@ public class EncerradorDeLeilaoTest {
 		verify(dao).atualiza(leilao1);
 	}
 	
+	@Test
+	public void naoDeveEncerrarLeiloesQueComecaramMenosDeUmaSemanaAtras() {
+		Calendar dataOntem = Calendar.getInstance();
+		dataOntem.add(Calendar.DAY_OF_MONTH, -1);
+		
+		Leilao leilao1 = new CriadorDeLeilao().para("TV de plasma").naData(dataOntem).constroi();
+		Leilao leilao2 = new CriadorDeLeilao().para("Geladeira").naData(dataOntem).constroi();
+		
+		LeilaoDao dao = mock(LeilaoDao.class);
+		
+		when(dao.correntes()).thenReturn(Arrays.asList(leilao1, leilao2));
+		
+		EncerradorDeLeilao encerradorDeLeilao =  new EncerradorDeLeilao(dao);
+		encerradorDeLeilao.encerra();
+		
+		assertEquals(0, encerradorDeLeilao.getTotalEncerrados());
+		assertFalse(leilao1.isEncerrado());
+		assertFalse(leilao2.isEncerrado());
+		
+		verify(dao, never()).atualiza(leilao1);
+		verify(dao, never()).atualiza(leilao2);
+	}
 }
